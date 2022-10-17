3afe502d5079065c4262293bed3d54744ee76dcf
itamavig <clingy-soybean.0z@icloud.com>
2022-10-17 13:36:23 +0300

    add group inheritance of id generator

--- group.py
+++ group.py
@@ -1,20 +1,22 @@
 from dataclasses import dataclass
 from typing import List
+
+from IIdentifiable import IIdentifiable
 from cell import Cell
 
+groups = dict()
 
-class Group:
-    _recent_id = -1
 
-    def __init__(self, cells: List[Cell], possibilities: List[int]):
+class Group(IIdentifiable):
+
+    def __init__(self, group_cells: List[Cell], possibilities: List[int]):
+        super().__init__()
         self.value_mapping = dict()
-        for cell in cells:
+        for cell in group_cells:
             self.value_mapping[cell.id] = 0
-
+        groups[self.id] = self
+        
     def _assign_value(self, cell_id, value):
         if value in self.value_mapping.values():
             raise Exception("Logical Flaw Error: Tried to assign a value in a group \n"
                             "in which the value already exists")
-
-
-
