63e2a2e52c40942e22dc289a087ff896bca8b72f
itamavig <clingy-soybean.0z@icloud.com>
2022-10-17 13:37:15 +0300

    add cell inheritance of id generator and create a cell dictionary

--- cell.py
+++ cell.py
@@ -2,31 +2,35 @@
 Class Cell.
 This object represents a cell in a board puzzle.
 """
+from IIdentifiable import IIdentifiable
+
+cells = dict()
 
 
-class Cell:
+class Cell(IIdentifiable):
     """
     This object represents a cell in a board puzzle.
     """
     _recent_id = -1
 
     def __init__(self, possibilities_num=9):
-        self.id = self._assign_id()
+        super().__init__()
         self.possibilities = [i for i in range(1, possibilities_num + 1)]
         self.value = 0
+        cells[self.id] = self
 
     def __str__(self):
         if self.value == 0:
             _str = f"Cell id = 0, \n" \
-                  f"Cell possible values = {self.possibilities}"
+                   f"Cell possible values = {self.possibilities}"
         else:
             _str = f"Cell id = 0, \n" \
-                  f"Cell value = {self.value}"
+                   f"Cell value = {self.value}"
         return _str
 
-    def _assign_id(self):
-        Cell._recent_id += 1
-        return self._recent_id
+    # def _assign_id(self):
+    #     Cell._recent_id += 1
+    #     return self._recent_id
 
     def _react_to_possibility_change(self):
         tmp = self.possibilities.pop()
