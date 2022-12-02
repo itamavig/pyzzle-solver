07693221b0abd9efa1769b47f596c2ed34a6b965
itamavig <clingy-soybean.0z@icloud.com>
2022-12-03 01:32:17 +0200

    change group cells reference, improve error messages, add cell value mapping method

--- group.py
+++ group.py
@@ -10,22 +10,33 @@ class Group(IIdentifiable):
 
     def __init__(self, group_cells: List[Cell], possibilities: List[int]):
         super().__init__()  # Creates Identifier for instance
-        self.value_mapping = dict()
-        for cell in group_cells:
-            self.value_mapping[cell.id] = 0
+        self.group_cells = group_cells
         GlobalParameters.all_groups[self.id] = self
 
     def _assign_value(self, cell_id, value):
-        value_exists_in_group = value in self.value_mapping.values()
-        cell_already_has_different_value = self.value_mapping.get(cell_id) != value
-        cell_already_has_this_value = self.value_mapping.get(cell_id) == value
+        value_exists_in_group = value in self._get_assigned_values().values()
+        cell_already_has_different_value = self._get_assigned_values().get(cell_id) != value
+        cell_already_has_this_value = self._get_assigned_values().get(cell_id) == value
         if value_exists_in_group:
-            raise Exception("Logical Flaw Error: Tried to assign a value in a group \n"
-                            "in which the value already exists")
+            cell_with_this_value_id = None
+            for c in self.group_cells:
+                if c.value == value:
+                    cell_with_this_value_id = c.id
+            raise Exception(f"Logical Flaw Error: Tried to assign value {value} in group {self.id},\n"
+                            f"but this value already exists in cell {cell_with_this_value_id}")
         if cell_already_has_different_value:
-            raise Exception("Logical Flaw Error: Tried to assign a value to a cell \n"
-                            "that already has a different value")
+            raise Exception(f"Logical Flaw Error: Tried to assign value {value} to cell {cell_id}\n"
+                            f"that already has the value {GlobalParameters.all_cells[cell_id].value}")
         if cell_already_has_this_value:
             print(f"Logical Redundancy Warning: program has been trying to assign \n"
                   f"to cell {cell_id} value {value} at least twice")
             return
+        GlobalParameters.all_cells[cell_id] = value
+
+    def _get_assigned_values(self):
+        current_group_values = dict()
+        for c in self.group_cells:
+            if c.value != 0:
+                current_group_values[c.id] = c.value
+        return current_group_values
+
