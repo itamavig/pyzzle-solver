ce14809bee61bc7b08c655f059c033b32bad5567
itamavig <clingy-soybean.0z@icloud.com>
2022-12-03 01:30:39 +0200

    change to typing Dict instead of built-in dict

--- common.py
+++ common.py
@@ -1,4 +1,9 @@
+from typing import Dict
+
+from cell import Cell
+from group import Group
+
 
 class GlobalParameters:
-    all_groups = dict()
-    all_cells = dict()
+    all_groups = Dict[int, Group]
+    all_cells = Dict[int, Cell]
