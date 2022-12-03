40e7a359f5e1cdb8d8c5f8ec9d8ddeea1c37f549
itamavig <clingy-soybean.0z@icloud.com>
2022-12-03 18:59:13 +0200

    add board implementation

--- board.py
+++ board.py
@@ -1,6 +1,10 @@
+import numpy as np
+
+from cell import Cell
 from IIdentifiable import IIdentifiable
 
 
 class Board(IIdentifiable):
     def __init__(self):
         super().__init__()
+        self.grid = np.ndarray(shape=(9, 9), dtype=Cell)
